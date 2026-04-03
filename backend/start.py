f5976c35c3402e2b7325c293b83a95f2ea3baf79
Lakshya Jha <lakshya2003jha@gmail.com>
2026-04-03 14:41:48 +0530

    Fix langchain error

--- backend/start.py
+++ backend/start.py
@@ -2,12 +2,12 @@ import uvicorn
 import os
 
 if __name__ == "__main__":
-    from dotenv import load_dotenv
-    load_dotenv()
+    # from dotenv import load_dotenv
+    # load_dotenv()
     
     port = int(os.environ.get("PORT", 8000))  
     uvicorn.run(
-        "main:app",
+        "backend.main:app",
         host="0.0.0.0",
         port=port,
         reload=False, 
